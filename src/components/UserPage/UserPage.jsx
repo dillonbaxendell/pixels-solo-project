25d86257bfc0042ef0cb3d1268ef057fd4aab9c6
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-06-28 11:47:38 -0500

    next button created (not working yet)

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+//Buttons
+import IconButton from '@material-ui/core/IconButton';
+import ArrowForwardIosOutlinedIcon from '@material-ui/icons/ArrowForwardIosOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 function UserPage() {
+  const classes = useStyles();
+  const history = useHistory();
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+
+  const handleNextPage = () => {
+    history.push('/MoodForm')
+  }
+
   return (
     <div className="container">
       <h2>Welcome, {user.first_name}!</h2>
       {/* <p>Your ID is: {user.id}</p> */}
+      <div>
+        <h4>An affirmation message will go here.</h4>
+      </div>
       <LogOutButton className="btn" />
+      <div className={classes.root}>
+      <IconButton >
+        <ArrowForwardIosOutlinedIcon onClick={handleNextPage}/>
+      </IconButton>
+      </div>
     </div>
   );
 }
