bb9bc0b22fcbc23235a655a4acbbd9881c4c740b
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-06-28 14:26:07 -0500

    moodValue working upon select option

--- src/components/ReflectionForm/ReflectionForm.jsx
+++ src/components/ReflectionForm/ReflectionForm.jsx
@@ -1,21 +1,24 @@
+import React, { useState } from 'react';
+
 function ReflectionForm() {
+    const [moodValue, setMoodValue] = useState('');
+
+    console.log('The moodValue is:', moodValue);
+
+
+
   return (
     <>
       <div>
         <h2>How are you doing today?</h2>
 
-        <select>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+        <select value={moodValue} onChange={(event) => setMoodValue(event.target.value)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
         </select>
-      </div>
-
-      <div>
-
-       
       </div>
     </>
   );
