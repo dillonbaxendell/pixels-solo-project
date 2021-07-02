810678717121ab4ea0583678dec2776047172692
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-07-02 16:45:23 -0500

    mood icons changing moodValue on click

--- src/components/MoodCheckIn/MoodCheckIn.jsx
+++ src/components/MoodCheckIn/MoodCheckIn.jsx
@@ -1,30 +1,36 @@
 import {useDispatch, useSelector} from 'react-redux';
 import NextButton from '../NextButton/NextButton';
+import mood1 from '../MoodCheckIn/MOOD1.JPEG';
+import mood2 from '../MoodCheckIn/MOOD2.JPEG';
+import mood3 from '../MoodCheckIn/MOOD3.JPEG';
+import mood4 from '../MoodCheckIn/MOOD4.JPEG';
+import mood5 from '../MoodCheckIn/MOOD5.JPEG';
+
 
 
 function MoodCheckIn() {
   const dispatch = useDispatch();
+  const moodIcons = [{value: 1, img: mood1}, {value: 2, img: mood2}, {value: 3, img: mood3}, {value: 4, img: mood4}, {value: 5, img: mood5}];
 
   const moodValue = useSelector(store => store.moodValue);
   console.log("The moodValue is:", moodValue);
 
+  const handleClick =(moodValue) => {
+    console.log('clicked an image');
+
+    dispatch({ type: 'SET_MOOD', payload: moodValue});
+  }
 
   return (
     <>
       <form>
         <div id="mood-check-in">
           <h2>How are you doing today?</h2>
-
-          <select
-            value={moodValue}
-            onChange={(event) => dispatch({type: 'SET_MOOD', payload: event.target.value })}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
+          <div>
+            {moodIcons.map( mood => {
+              return( <img value={mood.value} src={mood.img} width="75px" height="75px" onClick={() => handleClick(mood.value)}/>
+            )})}
+            </div>
 
           <NextButton pageRoute="/reflection/2" />
         </div>
