3c6cf63ad6d04293100b228354e4a0541181165b
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-07-10 18:47:41 -0500

    relationship router cleanup

--- server/routes/relationship.router.js
+++ server/routes/relationship.router.js
@@ -1,42 +1,51 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 /**
- * GET route template
+ * GET route
+ * grabs the users relationships (found in Activities Check In)
  */
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // GET route code here
   const queryText = `SELECT * FROM "relationship" WHERE "relationship".user_id = $1;`;
 
-  pool.query(queryText, [req.user.id])
-  .then( result => {
+  pool
+    .query(queryText, [req.user.id])
+    .then((result) => {
       res.send(result.rows);
-  })
-  .catch( error => {
-      console.log('Error in GET relationship.router.js', error);
-  })
+    })
+    .catch((error) => {
+      console.log("Error in GET relationship.router.js", error);
+    });
 });
 
 /**
- * POST route template
+ * POST route
+ * inserts the new relationship into the relationship table
  */
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // POST route code here
   const newRelationship = req.body;
-  console.log('newRelationship is:', newRelationship);
+  console.log("newRelationship is:", newRelationship);
 
-  const queryText = `INSERT INTO "relationship" ("name", "relationship_to_user", "relationship_icon", "user_id") VALUES ($1, $2, $3, $4);`
+  const queryText = `INSERT INTO "relationship" ("name", "relationship_to_user", "relationship_icon", "user_id") VALUES ($1, $2, $3, $4);`;
 
-  const values = [req.body.name, req.body.relationship_to_user, req.body.relationship_icon, req.body.user_id];
+  const values = [
+    req.body.name,
+    req.body.relationship_to_user,
+    req.body.relationship_icon,
+    req.body.user_id,
+  ];
 
-  pool.query(queryText, values)
-  .then( () => {
+  pool
+    .query(queryText, values)
+    .then(() => {
       res.sendStatus(201);
-  })
-  .catch( error => {
-      console.log('Error in postRelationship', error);
-  })
+    })
+    .catch((error) => {
+      console.log("Error in postRelationship", error);
+    });
 });
 
 module.exports = router;
