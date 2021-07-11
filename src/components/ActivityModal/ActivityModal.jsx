b05e5db00a5a4756ec5e3199b13eb2190b6a4153
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-07-11 10:14:53 -0500

    activity modal cleanup

--- src/components/ActivityModal/ActivityModal.jsx
+++ src/components/ActivityModal/ActivityModal.jsx
@@ -1,17 +1,19 @@
+//Imports
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import IconButton from "@material-ui/core/IconButton";
-import AddIcon from '@material-ui/icons/Add';
+import AddIcon from "@material-ui/icons/Add";
 import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import './ActivityModal.css';
+import "./ActivityModal.css";
 
+//Styles for chip components
 const useStyles = makeStyles((theme) => ({
   modal: {
     display: "flex",
@@ -24,29 +26,36 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
-  header : {
-    marginBottom: 30
+  header: {
+    marginBottom: 30,
   },
   closeBtn: {
     marginTop: 50,
-    float: 'left',
-  }
+    float: "left",
+  },
 }));
 
 export default function ActivityModal() {
   const classes = useStyles();
   const dispatch = useDispatch();
+
+  //local state
   const [open, setOpen] = useState(false);
   const [newActivity, setNewActivity] = useState("");
 
+  //When the button is clicked, open the modal
   const handleOpen = () => {
     setOpen(true);
   };
 
+  //On submit, do this:
   const handleSubmit = () => {
+    //Input validation: if the input is empty, alert the user
     if (newActivity === "") {
       alert("Please enter an activity or close this window");
-    } else {
+    }
+    //if it's not empty, add the activity
+    else {
       //Adds the activity to the database and activityList (initializes POST request)
       dispatch({
         type: "ADD_ACTIVITY",
@@ -59,10 +68,12 @@ export default function ActivityModal() {
         payload: newActivity,
       });
 
+      //close the modal
       setOpen(false);
     }
   };
 
+  //Close the modal
   const handleClose = () => {
     setOpen(false);
   };
@@ -70,7 +81,7 @@ export default function ActivityModal() {
   return (
     <div className="modal">
       <IconButton color="secondary" onClick={handleOpen}>
-      <Typography variant="button">Add Activity</Typography>
+        <Typography variant="button">Add Activity</Typography>
         <AddIcon />
       </IconButton>
 
@@ -89,7 +100,9 @@ export default function ActivityModal() {
         <Fade in={open}>
           <div className={classes.paper}>
             <div className={classes.header}>
-            <Typography variant="h4" id="add-activity">Add Activity</Typography>
+              <Typography variant="h4" id="add-activity">
+                Add Activity
+              </Typography>
             </div>
             <TextField
               value={newActivity}
@@ -97,14 +110,19 @@ export default function ActivityModal() {
               placeholder="activity name"
               onChange={(event) => setNewActivity(event.target.value)}
             />
-            <Button className="modal" variant="contained" color="primary" onClick={handleSubmit}>
+            <Button
+              className="modal"
+              variant="contained"
+              color="primary"
+              onClick={handleSubmit}
+            >
               ADD
             </Button>
-            <div >
-            <IconButton  className={classes.closeBtn} onClick={handleClose}>
-              <CloseIcon />
-              <Typography variant="button">cancel</Typography>
-            </IconButton>
+            <div>
+              <IconButton className={classes.closeBtn} onClick={handleClose}>
+                <CloseIcon />
+                <Typography variant="button">cancel</Typography>
+              </IconButton>
             </div>
           </div>
         </Fade>
