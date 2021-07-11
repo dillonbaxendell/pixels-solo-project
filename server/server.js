ab13b483d37fa64f5be505b70aee2443fdabeba7
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-07-11 09:49:50 -0500

    server.js cleanup

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 
-const sessionMiddleware = require('./modules/session-middleware');
-const passport = require('./strategies/user.strategy');
+const sessionMiddleware = require("./modules/session-middleware");
+const passport = require("./strategies/user.strategy");
 
 // Route includes
-const userRouter = require('./routes/user.router');
-const wordRouter = require('./routes/word.router');
-const activityRouter = require('./routes/activity.router');
-const relationshipRouter = require('./routes/relationship.router');
-const reflectionRouter = require('./routes/reflection.router');
-const moodRouter = require('./routes/mood.router');
+const userRouter = require("./routes/user.router");
+const wordRouter = require("./routes/word.router");
+const activityRouter = require("./routes/activity.router");
+const relationshipRouter = require("./routes/relationship.router");
+const reflectionRouter = require("./routes/reflection.router");
+const moodRouter = require("./routes/mood.router");
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -27,15 +27,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 /* Routes */
-app.use('/api/user', userRouter);
-app.use('/api/word', wordRouter);
-app.use('/api/activity', activityRouter);
-app.use('/api/relationship', relationshipRouter);
-app.use('/api/reflection', reflectionRouter);
-app.use('/api/mood', moodRouter)
+app.use("/api/user", userRouter);
+app.use("/api/word", wordRouter);
+app.use("/api/activity", activityRouter);
+app.use("/api/relationship", relationshipRouter);
+app.use("/api/reflection", reflectionRouter);
+app.use("/api/mood", moodRouter);
 
 // Serve static files
-app.use(express.static('build'));
+app.use(express.static("build"));
 
 // App Set //
 const PORT = process.env.PORT || 5000;
