c362f1b8f93cad6a60f37621477fde4dbd848928
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-07-11 11:36:01 -0500

    some info added on info page

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -7,9 +9,21 @@ import React from 'react';
 
 function InfoPage() {
   return (
-    <div className="container">
-      <p>Info Page</p>
-    </div>
+    <Container>
+      <Typography variant="h3">Thank You!</Typography>
+      <div>
+        <Typography variant="subtitle1">Technologies used:</Typography>
+        <ul>
+          <li>React</li>
+          <li>Redux</li>
+          <li>Saga</li>
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>PostgreSQL</li>
+          <li>Chart.js</li>
+        </ul>
+      </div>
+    </Container>
   );
 }
 
